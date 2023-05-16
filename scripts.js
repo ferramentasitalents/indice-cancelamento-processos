ac02e3ebcb13fb45d70702ad107b79a4f4bdc086
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 15:30:55 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateIndex() {
+  const numConcluded = parseFloat(document.getElementById("num-concluded").value);
+  const numCanceled = parseFloat(document.getElementById("num-canceled").value);
+  const index = (numCanceled / (numConcluded + numCanceled)) * 100;
+  document.getElementById("result").innerHTML = `Índice de cancelamento de pedidos de abertura de processos de R&S: ${index.toFixed(2)}%`;
+}
 
